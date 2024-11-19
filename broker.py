7144f4377f0a88afa0dbd9c562a1b275bd669d5d
Lorenzo Calisti <djonline96@gmail.com>
2024-11-19 16:33:39 +0100

    Move script in main function
    This commit moves all the script code inside a main funzion called when the module is the main module.
    This commit also adds a funcion `get_env` that reads a variable from the current environment.

--- broker.py
+++ broker.py
@@ -1,11 +1,18 @@
 import feedparser
 import requests
+import os
 
-# Config
-FEED_URL = "feed_url"
-BOT_TOKEN = "bot:token"
-TG_CHAT_ID = "@chat_id"
-TG_URL = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+def get_env(env: str) -> str:
+    """
+    Read the vale of given environment variable or throw an error.
+
+    Params:
+        env: Name of the env variable to load.
+    """
+    val = os.getenv(env)
+    if val == None:
+        raise Exception(f"environment variable '{env}' not defined")
+    return val
 
 def send_item_to_tg(item: object, url: str, chat_id: str) -> bool:
     """
@@ -29,10 +36,28 @@ def send_item_to_tg(item: object, url: str, chat_id: str) -> bool:
         return False
     return True
 
-# Reed and parse the RSS the feeds
-feed = feedparser.parse(FEED_URL)
-print(f"got {len(feed.entries)} feeds")
+def main():
+    # Get configs from env variables
+    FEED_URL = get_env('FEED_URL')
+    TG_BOT_TOKEN = get_env('TG_BOT_TOKEN')
+    TG_CHAT_ID = get_env('TG_CHAT_ID')
+    TG_URL = f"https://api.telegram.org/bot{TG_BOT_TOKEN}/sendMessage"
 
-# Send new feeds to tg
-for entry in feed.entries:
-    send_item_to_tg(entry, TG_URL, TG_CHAT_ID)
+    print(f"{FEED_URL     = }")
+    print(f"{TG_BOT_TOKEN = }")
+    print(f"{TG_CHAT_ID   = }")
+    print(f"{TG_URL   = }")
+
+    # Reed and parse the RSS the feeds
+    feed = feedparser.parse(FEED_URL)
+    print(f"got {len(feed.entries)} feeds")
+
+    # Send new feeds to tg
+    for entry in feed.entries:
+        send_item_to_tg(entry, TG_URL, TG_CHAT_ID)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as ex:
+        print(f"ERROR: {ex}")
